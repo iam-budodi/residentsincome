66e4aa8e0b5ce4ce119f6e7e19acf7180baa4c52
Japhet Sebastian <japhet.sebastian01@gmail.com>
2022-05-22 19:17:39 +0300

    fixes the path params and restful request methods

--- src/main/java/com/japhet/application/residentincome/rest/IncomeEndpoint.java
+++ src/main/java/com/japhet/application/residentincome/rest/IncomeEndpoint.java
@@ -12,7 +12,11 @@ import jakarta.inject.Inject;
 import jakarta.persistence.OptimisticLockException;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -50,6 +54,8 @@ public class IncomeEndpoint {
 		return Response.ok(incomeClass).build();
 	}
 
+	@POST
+	@Consumes(APPLICATION_JSON)
 	public Response createIncomeClass(@NotNull IndividualIncome incomeClass,
 				@Context() UriInfo uriInfo) {
 		try {
@@ -64,7 +70,9 @@ public class IncomeEndpoint {
 		return Response.created(createdUri).build();
 	}
 
-	public Response deleteIncomeClass(@NotNull Long incomeClassId) {
+	@DELETE
+	@Path("{id: \\d+}")
+	public Response deleteIncomeClass(@PathParam("id") @NotNull @Min(1) Long incomeClassId) {
 		IndividualIncome incomeClass = findIncomeClass(incomeClassId);
 		if (incomeClass == null)
 			return Response.status(Response.Status.NOT_FOUND).build();
@@ -74,7 +82,10 @@ public class IncomeEndpoint {
 		return Response.status(Response.Status.NO_CONTENT).build();
 	}
 
-	public Response updateIncomeClass(@NotNull Long incomeClassId,
+	@PUT
+	@Path("{id: \\d+}")
+	@Consumes(APPLICATION_JSON)
+	public Response updateIncomeClass(@PathParam("id") @NotNull @Min(1) Long incomeClassId,
 				@NotNull IndividualIncome incomeClass) {
 		if (!incomeClassId.equals(incomeClass.getId())) {
 			return Response.status(Response.Status.CONFLICT).build();
