6e0f633a793bf49f35736342e5b21265e6020899
Japhet Sebastian <japhet.sebastian01@gmail.com>
2022-05-22 19:10:35 +0300

    Initialize JAX-RS application

---
+++ src/main/java/com/japhet/application/residentincome/rest/JAXRSApplication.java
@@ -0,0 +1,9 @@
+package com.japhet.application.residentincome.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JAXRSApplication extends Application {
+
+}
